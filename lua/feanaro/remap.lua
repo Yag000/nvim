70c42afe3b42fa7e37f6378120148ed66c8793bd
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2023-10-21 14:10:18 +0200

    fix(typos): fix typos in remap.lua

--- lua/feanaro/remap.lua
+++ lua/feanaro/remap.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Imporve J command
+-- Improve J command
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- Half page jumps stay in the middle of the page
@@ -16,7 +16,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Avoid copy of edleated selection
+-- Avoid copy of deleted selection
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -32,4 +32,5 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Open tmux sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
