4bb04e3e738141a1614a14ba9b92f89044d95752
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2024-02-03 11:49:36 +0100

    feat: Add lean support

--- lua/feanaro/lazy.lua
+++ lua/feanaro/lazy.lua
@@ -162,6 +162,26 @@ local plugins = {
 			"tex",
 		},
 	},
+
+	-- Lean
+	{
+		"Julian/lean.nvim",
+		event = { "BufReadPre *.lean", "BufNewFile *.lean" },
+
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"nvim-lua/plenary.nvim",
+			-- you also will likely want nvim-cmp or some completion engine
+		},
+
+		-- see details below for full configuration options
+		opts = {
+			lsp = {
+				on_attach = on_attach,
+			},
+			mappings = true,
+		},
+	},
 }
 
 require("lazy").setup(plugins, {})
